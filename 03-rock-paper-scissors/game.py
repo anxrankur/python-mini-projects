b7babf007f559b5ca6785cccfae8a597a97e9687
Ankur Shukla <thequent7@gmail.com>
2026-08-05 16:17:51 +0530

    Add Rock Paper Scissors Game

---
+++ 03-rock-paper-scissors/game.py
@@ -0,0 +1,21 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user = input("Choose rock, paper or scissors: ").lower()
+computer = random.choice(choices)
+
+print("Computer chose:", computer)
+
+if user == computer:
+    print("It's a draw!")
+elif (
+    (user == "rock" and computer == "scissors") or
+    (user == "paper" and computer == "rock") or
+    (user == "scissors" and computer == "paper")
+):
+    print("You win!")
+elif user in choices:
+    print("Computer wins!")
+else:
+    print("Invalid choice!")
